f6acee9bc9da12100bc26dadc477e1e5b44ebd42
zda58 <107224274+zda58@users.noreply.github.com>
2025-05-21 17:50:48 -0400

    5.2.4

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+const app = express()
+Lab5(app)
+Hello(app)
+app.listen(process.env.PORT || 4000)
