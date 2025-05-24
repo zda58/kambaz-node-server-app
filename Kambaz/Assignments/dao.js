5bb09967c6a62c3be472b1b141b23d27ba51b49c
zda58 <107224274+zda58@users.noreply.github.com>
2025-05-24 15:35:23 -0400

    a5

---
+++ Kambaz/Assignments/dao.js
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from "uuid";
+import Database from "../Database/index.js";
+
+export function createAssignment(assignment) {
+  const newAssignment = { ...assignment, _id: uuidv4() };
+  Database.assignments = [...Database.assignments, newAssignment];
+  return newAssignment;
+}
+
+export function getAssignmentsForCourse(courseId) {
+  const { assignments } = Database;
+  return assignments.filter((assignment) => assignment.course === courseId);
+}
+
+export function updateAssignment(assignmentId, assignmentUpdates) {
+  const { assignments } = Database;
+  const assignment = assignments.find((assignment) => assignment._id === assignmentId);
+  Object.assign(assignment, assignmentUpdates);
+  return assignment;
+}
+
+export function deleteAssignment(assignmentId) {
+  const { assignments } = Database;
+  Database.assignments = assignments.filter((assignment) =>
+    assignments._id != assignmentId);
+}
---
+++ Kambaz/Enrollments/dao.js
@@ -0,0 +1,15 @@
+import Database from "../Database/index.js";
+import { v4 as uuidv4 } from "uuid";
+
+export function enrollUserInCourse(userId, courseId) {
+  const { enrollments } = Database;
+  enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+  return enrollments[enrollments.length - 1]
+}
+
+export function deleteEnrollment(enrollmentId) {
+  const { enrollments } = Database;
+  Database.enrollments = enrollments.filter(
+    (enrollment) => enrollment._id !== enrollmentId
+  );
+}
